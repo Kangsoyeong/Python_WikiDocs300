677d794f6d891c0046c49017bd3554b02c5eb51d
Kang soyeong <dacapo@hufs.ac.kr>
2022-02-02 20:26:46 +0900

    007_설명 수정

--- 007_print 기초.py
+++ 007_print 기초.py
@@ -2,6 +2,7 @@
 #naver;kakao;sk;samsung
 
 print("naver", "kakao", "sk", "samsung", sep=";")
+
 #print 함수의 sep 인자로 ";"를 입력하면 출력되는 값들 사이에 한 칸의 공백대신 세미콜론이 출력된다.
 #아래의 코드도 동일한 결과가 나온다. 그러나 sep을 이용하면 좀 더 깔끔해보이고, 문자열 사이에 어떤 문자가 들어가있는지 알아보기 쉽다.
 #print("naver;kakao;sk;samsung")
