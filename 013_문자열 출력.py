7cf792b19700963dd94adea0670c4ec6f6d4185d
Kang soyeong <korea020808@naver.com>
2022-02-03 23:13:59 +0900

    013

---
+++ 013_문자열 출력.py
@@ -0,0 +1,15 @@
+#변수 s와 t에는 각각 문자열이 바인딩 되어있습니다.
+
+#>> s = "hello"
+#>> t = "python"
+#두 변수를 이용하여 아래와 같이 출력해보세요.
+
+#실행 예:
+#hello! python
+
+s = "hello"
+t = "python"
+print(s + "!" + " " + t)
+
+#아래와 같이 print 코드를 작성하면 좀 더 깔끔하다.
+#print(s+"!", t)
