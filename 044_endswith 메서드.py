1c09c08b73be373b683c8deb876e18a6c71c3c12
Kang soyeong <korea020808@naver.com>
2022-02-07 16:05:11 +0900

    044

---
+++ 044_endswith 메서드.py
@@ -0,0 +1,9 @@
+#파일 이름이 문자열로 저장되어 있을 때 endswith 메서드를 사용해서 파일 이름이 'xlsx'로 끝나는지 확인해보세요.
+#file_name = "보고서.xlsx"
+
+file_name = "보고서.xlsx"
+print(file_name.endswith("xlsx"))
+
+#출력결과 : True
+#endswith 메서드는 문자열이 특정 문자로 끝나는지에 대해 알려준다.
+#True나 False를 반환한다.
