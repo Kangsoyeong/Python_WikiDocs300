37cb292e4da710b0850ab28627fd89fb81fc0578
Kang soyeong <korea020808@naver.com>
2022-02-07 15:47:59 +0900

    028

---
+++ 028_문자열은 immutable.py
@@ -0,0 +1,9 @@
+#아래 코드의 실행 결과를 예상해보세요.
+#>> lang = 'python'
+#>> lang[0] = 'P'
+#>> print(lang)
+
+정답 : 문자열은 수정할 수 없다.
+#immutable : 변경할 수 없는, 불변의
+#실행 결과를 확인해보면 문자열이 할당(assignment) 메서드를 지원하지 않음을 알 수 있다.
+#TypeError: 'str' object does not support item assignment
