03ccee7cb1b5e7edc804a36cf7c7110142254dbd
Kang soyeong <korea020808@naver.com>
2022-02-07 16:06:59 +0900

    046

---
+++ 046_startswith 메서드.py
@@ -0,0 +1,8 @@
+#파일 이름이 문자열로 저장되어 있을 때 startswith 메서드를 사용해서 파일 이름이 '2020'로 시작하는지 확인해보세요.
+#file_name = "2020_보고서.xlsx"
+
+file_name = "2020_보고서.xlsx"
+print(file_name.startswith('2020'))
+
+#출력결과 : True
+#startswith 메서드는 문자열이 특정 문자로 시작하는지에 대해 알려준다.
