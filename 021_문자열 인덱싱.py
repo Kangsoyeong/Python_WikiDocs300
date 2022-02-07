c4cfe86fc6f40c9e230029d7ecba23e1fd6c5329
Kang soyeong <korea020808@naver.com>
2022-02-07 15:42:55 +0900

    021

---
+++ 021_문자열 인덱싱.py
@@ -0,0 +1,11 @@
+#letters가 바인딩하는 문자열에서 첫번째와 세번째 문자를 출력하세요.
+
+#>> letters = 'python'
+#실행 예
+#p t
+
+letters = 'python'
+print(letters[0], letters[2])
+
+#인덱싱(Indexing)이란 무엇인가를 ‘가르킨다’라는 의미이다.
+#대괄호 안에 숫자를 넣어 인덱싱을 할 수 있으며, 파이썬 인덱싱은 0부터 시작한다.
